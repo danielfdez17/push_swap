8e0ce7e46acb89904939eb9dc8004ce3ba119f56
danielfdez17 <daniel17dev@gmail.com>
2025-10-31 17:45:13 +0100

    fix: random numbers generator doesn't generate repeated number

--- generator.py
+++ generator.py
@@ -2,15 +2,17 @@ import random
 
 
 def generate_random_numbers(min_val, max_val):
-	"""Return a randomly shuffled list of integers from min_val to max_val inclusive."""
+	"""Return a randomly shuffled list of unique integers from min_val to max_val inclusive."""
 	if min_val > max_val:
 		min_val, max_val = max_val, min_val
-	return random.sample(range(min_val, max_val + 1), k=(max_val - min_val + 1))
+	nums = list(range(min_val, max_val + 1))
+	random.shuffle(nums)
+	return nums
 
 def main(min_val, max_val):
 	"""Main entry: takes min and max as parameters and returns the random list."""
 	return generate_random_numbers(min_val, max_val)
 
 if __name__ == "__main__":
-	numbers = main(0, 10)
+	numbers = main(0, 500)
 	print(" ".join(map(str, numbers)))
