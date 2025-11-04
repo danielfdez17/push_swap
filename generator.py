f6f7e9ef25f9095d68fa215d8eb384d37a2dd557
danfern3 <danfern3@student.42madrid.com>
2025-11-04 12:53:23 +0100

    feat: roxy's numbers generator

--- generator.py
+++ generator.py
@@ -1,18 +1,23 @@
 import random
 
 
-def generate_random_numbers(min_val, max_val):
-	"""Return a randomly shuffled list of unique integers from min_val to max_val inclusive."""
-	if min_val > max_val:
-		min_val, max_val = max_val, min_val
-	nums = list(range(min_val, max_val + 1))
-	random.shuffle(nums)
-	return nums
+def grn(min_val, max_val):
+	nums = random.sample(range(500),500)
+	print(*nums)
+
+# def generate_random_numbers(min_val, max_val):
+# 	"""Return a randomly shuffled list of unique integers from min_val to max_val inclusive."""
+# 	if min_val > max_val:
+# 		min_val, max_val = max_val, min_val
+# 	nums = list(range(min_val, max_val + 1))
+# 	random.shuffle(nums)
+# 	return nums
 
 def main(min_val, max_val):
 	"""Main entry: takes min and max as parameters and returns the random list."""
-	return generate_random_numbers(min_val, max_val)
+	grn(min_val, max_val)
+	# return generate_random_numbers(min_val, max_val)
 
 if __name__ == "__main__":
 	numbers = main(0, 500)
-	print(" ".join(map(str, numbers)))
+	# print(" ".join(map(str, numbers)))
