64ec69e70c62884de72d785df35d89aa0ef75eb3
danielfdez17 <daniel17dev@gmail.com>
2026-03-02 13:26:39 +0100

    feat(tester): finish push_swap tester

--- tester.sh
+++ tester.sh
@@ -7,7 +7,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
+BLUE='\033[0;36m'
 ORANGE='\033[0;33m'
 NC='\033[0m' # No Color
 WARNING=$YELLOW'[WARNING]'
@@ -36,65 +36,80 @@ log_todo() {
 	echo $TODO "$1" $NC
 }
 
-make all --no-print-directory
-# make re --no-print-directory
+clear
+log_warning "Make sure ./push_swap is up to date"
 
 generate_numbers() {
 	if [ -z "$1" ]; then
 		max=100
+	else
+		max=$1
 	fi
-	max=$1
 	shuf -i 1-$max -n $max | tr "\n" " "
 }
 
 push_swap() {
-	# numbers=$(generate_numbers $1)
-	# if [ -z "$2" ]; then
-	# 	echo $numbers
-	# fi
+	numbers=$(generate_numbers $1)
+	echo -n $numbers " "
 	# log_info "Testing with $1 numbers..."
-	movs=$(generate_numbers $1 | xargs ./push_swap | wc -l)
+	movs=$(echo $numbers | xargs ./push_swap | wc -l)
 	# log_ok "Number of moves: $movs"
-	echo $movs
+	return $movs
 }
 
-test() {
-	log_info "Running tests..."
-	push_swap 6
-	push_swap 10
-	push_swap 100
+test_n() {
+	n=$1
+	limit=$2
+	ok_iterations=0
+	bad_iterations=0
+	log_info "Running $iterations iterations with $n numbers..."
+	for i in $(seq 1 $iterations); do
+		numbers=$(push_swap $n)
+		moves=$?
+		# Check if the number of moves is greater than 12
+		if [ $moves -gt $limit ]; then
+			log_error "$numbers $moves moves (exceeds $limit)"
+			bad_iterations=$((bad_iterations + 1))
+		else
+			log_ok "$moves moves (within $limit)"
+			ok_iterations=$((ok_iterations + 1))
+		fi
+	done
+	log_info "Summary for $n numbers: $GREEN$ok_iterations OK$NC, $RED$bad_iterations KO$BLUE out of $total_iterations iterations."
 }
 
-if [ -z "$1" ]; then
-	log_warning "No argument provided. Defaulting to 100."
-	read -p "Enter the number of iterations (default: 100): " iterations
-	if [ -z "$iterations" ]; then
-		iterations=100
+start() {
+	# Check if at least 2 integer arguments were provided
+	if [ $n -le 6 ]; then
+		test_n $n 12
+	elif [ $n -le 100 ]; then
+		test_n $n 700
+	elif [ $n -le 500 ]; then
+		test_n $n 5500
+	else
+		test_n $n $n
 	fi
+}
+
+# Check if ./push_swap exists
+if [ ! -f "./push_swap" ]; then
+	log_error "Error: ./push_swap not found. Please compile the project first."
+	exit 1
+fi
+
+# Check if the first argument is provided, if not set it to 100
+if [ -z $1 ]; then
+	iterations=100
 else
 	iterations=$1
 fi
 
-total_iterations=$iterations
-ok_iterations=0
-bad_iterations=0
-log_info "Running $iterations iterations with 6 numbers..."
-for i in $(seq 1 $iterations); do
-	moves=$(push_swap 6)
-	# Check if the number of moves is greater than 12
-	if [ $moves -gt 12 ]; then
-		# log_error "Iteration $i: $moves moves (exceeds 12)"
-		bad_iterations=$((bad_iterations + 1))
-	else
-		# log_ok "Iteration $i: $moves moves"
-		ok_iterations=$((ok_iterations + 1))
-	fi
-done
-percentage=$(awk "BEGIN {printf \"%.2f\", ($ok_iterations/$total_iterations)*100}")
-log_ok "$percentage% iterations passed"
-percentage=$(awk "BEGIN {printf \"%.2f\", ($bad_iterations/$total_iterations)*100}")
-log_error "$percentage% iterations failed"
+# Check if the second argument is provided, if not set it to 100
+if [ -z $2 ]; then
+	n=100
+else
+	n=$2
+fi
 
-
-# push_swap 10
-# push_swap 100
+log_info "Starting tests with $iterations iterations and $n numbers..."
+start
